8a33efa625fc2ffdb4665ef1ff60e7b1a92479f4
romanm <roman.mishchenko@gmail.com>
2014-09-18 13:58:15 +0300

    db dump

---
+++ db-init.sql
@@ -0,0 +1,9 @@
+create table drug1 
+(drug_id int PRIMARY KEY auto_increment, drug_name varchar(50) NOT NULL UNIQUE);
+
+create table patient1 
+(patient_id int PRIMARY KEY auto_increment, patient_name varchar(50) NOT NULL UNIQUE);
+
+-- sql db dump command
+-- script to '/home/roman/Documents/01_curepathway/work3/cuwy-cpoe-hol2/cuwy-cpoe-hol1.db.sql'
+
